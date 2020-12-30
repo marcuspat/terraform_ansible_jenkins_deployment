a5324e7b977ef95dd39146b4cf91ad74211cf4e4
marcuspat <marcus.patman@gmail.com>
2020-12-30 17:02:58 +0000

    updated files

--- alb.tf
+++ alb.tf
@@ -40,10 +40,28 @@ resource "aws_lb_listener" "jenkins-listener-http" {
   port              = "80"
   protocol          = "HTTP"
   default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app-lb-tg.id
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
   }
 }
+
+resource "aws_lb_listener" "jenkins-listener-https" {
+  provider          = aws.region-master
+  load_balancer_arn = aws_lb.application-lb.arn
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn   = aws_acm_certificate.jenkins-lb-https.arn
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app-lb-tg.arn
+  }
+}
+
 
 
 resource "aws_lb_target_group_attachment" "jenkins-master-attach" {
--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,7 @@ output "Jenkins-Worker-Public-IPs" {
 output "LB-DNS-NAME" {
   value = aws_lb.application-lb.dns_name
 }
+
+output "url" {
+  value = aws_route53_record.jenkins.fqdn
+}
